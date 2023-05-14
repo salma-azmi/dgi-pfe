fcf5653714c4294856772229c250ef99baf5ba2e
Mohamed Mhamdi <Mohamed.Mhamdi@fan4all.com>
2023-05-14 11:53:17 +0100

    init

---
+++ back_dgi/src/main/java/com/example/back_dgi/BackDgiApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_dgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackDgiApplication.class, args);
+    }
+
+}
